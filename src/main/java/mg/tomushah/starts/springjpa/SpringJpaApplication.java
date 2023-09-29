e354e17d28e1b80b596554cd081682fcc3a2898b
tonymushah <tonyramanantsalama@gmail.com>
2023-09-29 12:25:43 +0300

    Init Project

---
+++ src/main/java/mg/tomushah/starts/springjpa/SpringJpaApplication.java
@@ -0,0 +1,13 @@
+package mg.tomushah.starts.springjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJpaApplication.class, args);
+	}
+
+}
